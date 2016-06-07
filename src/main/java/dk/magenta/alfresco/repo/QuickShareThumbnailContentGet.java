c09ee62b15cc61e04946119b9807dba890a6f5ec
Seth Yastrov <syastrov@gmail.com>
2016-06-07 09:26:16 +0200

    Initial attempt

--- src/main/java/dk/magenta/alfresco/repo/QuickShareThumbnailContentGet.java
+++ src/main/java/dk/magenta/alfresco/repo/QuickShareThumbnailContentGet.java
@@ -2,9 +2,11 @@ package dk.magenta.alfresco.repo;
 
 
 import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.thumbnail.ThumbnailService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.extensions.webscripts.WebScriptRequest;
@@ -23,16 +25,27 @@ public class QuickShareThumbnailContentGet extends
         protected NodeService myNodeService;
         protected ContentService myContentService;
 
+        @Override
         protected void streamContentLocal(WebScriptRequest req, WebScriptResponse res, NodeRef nodeRef, boolean attach, QName propertyQName, Map<String, Object> model) throws IOException
         {
+                System.out.println("FOOBAR!");
                 String userAgent = req.getHeader("User-Agent");
                 userAgent = userAgent != null ? userAgent.toLowerCase() : "";
                 boolean rfc5987Supported = (userAgent.contains("msie") || userAgent.contains(" trident/") || userAgent.contains(" chrome/") || userAgent.contains(" firefox/"));
 
                 if (attach && rfc5987Supported)
                 {
+                        ChildAssociationRef childAssociationRef = myNodeService
+                                .getPrimaryParent(nodeRef);
+                        NodeRef originalNodeRef = childAssociationRef
+                                .getParentRef();
+
+                        System.out.println("Orgin nodeRef: !" + originalNodeRef);
+                        // Use the filename of the original nodeRef, NOT the
+                        // thumbnail's name.
                         String name = (String) myNodeService.getProperty
-                                (nodeRef, ContentModel.PROP_NAME);
+                                (originalNodeRef, ContentModel.PROP_NAME);
+                        System.out.println("Orig name: " + name);
 
                         // IE use file extension to get mimetype
                         // So we set correct extension. see MNT-11246
